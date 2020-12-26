d10a4b7a09bd8bbbb2b08303bdd931b146a1d9aa
ainertec <57117318+Ainertec@users.noreply.github.com>
2020-12-26 05:51:52 -0300

    new update

---
+++ executaveis_modulos/HotelLumiar.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	
+	//faz backup a pasta indicada
+	system("cd C:\\hotellumiar-x64\\frontend && yarn start");
+	
+	return 0;
+}
---
+++ executaveis_modulos/node.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	
+	system("cd C:\\hotellumiar-x64\\backend && node src\\server.js");
+	
+	return 0;
+}
